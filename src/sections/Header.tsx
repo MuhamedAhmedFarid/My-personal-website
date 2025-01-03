8b94721ce0c6b67389e47706e57126a4495747fa
mohamed <mohamed@gmail.com>
2025-01-03 21:32:45 +0200

    test

--- src/sections/Header.tsx
+++ src/sections/Header.tsx
@@ -21,7 +21,7 @@ export const Header = () => {
           onSetActive={() => setSelectedTab("home")}
           className={`nav-item ${selectedTab === "home" && "nav-active"}`}
         >
-          Home
+          Homeeeeeeeeee
         </ScrollLink>
         <ScrollLink
           spy={true}
