60d477aa46b709fed927ceccba7b54a898832eee
mohamed <mohamed@gmail.com>
2025-01-03 20:33:16 +0200

    links added

--- src/sections/Footer.tsx
+++ src/sections/Footer.tsx
@@ -1,7 +1,7 @@
 import ArrowUpRightIcon from "@/assets/icons/arrow-up-right.svg";
 const footerLinks = [
   {
-    title: "Youtube",
+    title: "Facebook",
     href: "#",
   },
   {
@@ -10,16 +10,16 @@ const footerLinks = [
   },
   {
     title: "Instgram",
-    href: "#",
+    href: "https://www.instagram.com/moham_edahmed1664/profilecard/?igsh=MXFsZjllOHR3OXZrZg%3D%3D",
   },
   {
     title: "LinkedIn",
-    href: "#",
+    href: "https://www.linkedin.com/in/muhamed-ahmed-64551833b?utm_source=share&utm_campaign=share_via&utm_content=profile&utm_medium=android_app",
   },
 ];
 export const Footer = () => {
   return (
-    <footer className="relative -z-10 overflow-x-clip">
+    <footer className="relative  overflow-x-clip">
       <div className="absolute h-[400px] w-[1600px] bottom-0 left-1/2 -translate-x-1/2 bg-emerald-300/30 [mask-image:radial-gradient(50%_50%_at_bottom_center,black,transparent)] -z-10"></div>
       <div className="container">
         <div className="border-t border-white/15 py-6 text-sm flex flex-col md:flex-row md:justify-between items-center gap-8">
@@ -27,9 +27,10 @@ export const Footer = () => {
           <nav className="flex flex-col items-center gap-8 md:flex-row ">
             {footerLinks.map((link) => (
               <a
+              target="_blank"
                 href={link.href}
                 key={link.title}
-                className="inline-flex items-center gap-1.5"
+                className="inline-flex items-center gap-1.5 cursor-pointer"
               >
                 <span className="font-semibold">{link.title}</span>
                 <ArrowUpRightIcon className="size-4" />
