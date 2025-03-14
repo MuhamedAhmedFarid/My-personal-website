411fd58041bbea3f81e7e9abab8873cd9970efbb
mohamed <mohamed@gmail.com>
2025-03-14 02:18:40 +0200

    small change

--- src/sections/Projects.tsx
+++ src/sections/Projects.tsx
@@ -63,7 +63,7 @@ export const ProjectsSection = () => {
               <div className="lg:pb-16">
                 <div className="bg-gradient-to-r from-emerald-300 to-sky-400 gap-2 inline-flex font-bold uppercase tracking-widest text-sm text-transparent bg-clip-text">
                 <span>{project.company}</span>
-                <span>&bull;</span>
+                <span>.</span>
                 <span>{project.year}</span>
               </div>
 
